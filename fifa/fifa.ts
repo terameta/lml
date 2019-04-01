4070780613023d01908566e208ba8e5668be6688
AliRiza Dikici <AliRiza.Dikici@kerzner.com>
2019-04-01 18:04:07 +0400

    Splitting

--- fifa/fifa.ts
+++ fifa/fifa.ts
@@ -4,7 +4,17 @@ import { readFile } from 'fs';
 const readFileP = promisify(readFile);
 
 const start = async () => {
-	console.log(await readFileP('./datafiles/fifa19data.csv','utf8'));
+	const data = (await readFileP('./datafiles/fifa19data.csv', 'utf8')).trim().split('\n').map(r => r.split(','));
+	const headers = data.splice(0, 1)[0];
+	headers[0] = 'Row Number';
+	const messi = data[0];
+	const ronal = data[1];
+	const neyma = data[2];
+	console.clear();
+	for (let i = 0; i < 30; i++) {
+		console.log(i, headers[i], messi[i], ronal[i], neyma[i]);
+	}
+	console.log('Dates are also spliting. So this is not good');
 }
 
 start().then(() => {
