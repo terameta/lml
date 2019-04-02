98f90a979a6eaee373561188b4e8c14cab1d3107
AliRiza Dikici <AliRiza.Dikici@kerzner.com>
2019-04-02 13:16:27 +0400

    SortBy function implemented

--- fifa/fifa.ts
+++ fifa/fifa.ts
@@ -1,10 +1,12 @@
 import { readCSV } from '../utilities/readcsv';
+import { sortBy } from '../utilities/sorters';
 
 
 const start = async () => {
 	const results = await readCSV('./datafiles/fifa19data.csv', { mapHeaders: ({ header, index }) => index === 0 ? 'RowNum' : header });
 	// console.log(results[0]);
-	console.log(results.map(r => [r.Name, r.Age, r.Wage]));
+	// console.log(results.map(r => [r.Name, r.Age, r.Wage]).sort(sortBy(1)));
+	console.log(results.sort(sortBy('Name', true)).map(r => [r.Name, r.Age, r.Wage]));
 }
 
 start().then(() => {
