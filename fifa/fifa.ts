f3057793987b7126b12a4bf66911055fecd0e490
AliRiza Dikici <AliRiza.Dikici@kerzner.com>
2019-04-02 16:30:01 +0400

    KNN Finished

--- fifa/fifa.ts
+++ fifa/fifa.ts
@@ -1,12 +1,33 @@
 import { readCSV } from '../utilities/readcsv';
 import { sortBy } from '../utilities/sorters';
+import * as _ from 'lodash';
 
+const k = 5;
+const predictionPoint = 100;
+
+function distance(point: number) {
+	return Math.abs(point - predictionPoint);
+}
 
 const start = async () => {
 	const results = await readCSV('./datafiles/fifa19data.csv', { mapHeaders: ({ header, index }) => index === 0 ? 'RowNum' : header });
-	// console.log(results[0]);
-	// console.log(results.map(r => [r.Name, r.Age, r.Wage]).sort(sortBy(1)));
-	console.log(results.sort(sortBy('Name', true)).map(r => [r.Name, r.Age, r.Wage]));
+	// // console.log(results[0]);
+	// // console.log(results.map(r => [r.Name, r.Age, r.Wage]).sort(sortBy(1)));
+	console.clear();
+	// console.log(results.map(r => [distance(r.ShotPower), r['International Reputation'], r.Name]).sort(sortBy(0)).slice(0, k));
+	// console.log('\n\n\n---------------------\n');
+	console.log(
+		_.chain(results)
+			.map(r => [distance(r.ShotPower), r['International Reputation'], r.Name])
+			.sortBy(r => r[0])
+			.slice(0, k)
+			.countBy(r => r[1])
+			.toPairs()
+			.sortBy(r => r[1])
+			.last()
+			.first()
+			.value()
+	);
 }
 
 start().then(() => {
--- utilities/sorters.ts
+++ utilities/sorters.ts
@@ -2,8 +2,8 @@ export const sortBy = (prop: any, desc = false) => {
   const sorter = (a: any, b: any) => {
     const multiplier = desc ? -1 : 1;
     if (a[prop] > b[prop]) return 1 * multiplier;
-    if (a[prop] < b[prop]) return -1 * multiplier;
-    return 0;
+    // if (a[prop] < b[prop]) return -1 * multiplier;
+    return -1 * multiplier;
   }
   return sorter;
 }
