b2ed6cc9efcb1b358a89d4ea485df473ca294c0e
AliRiza Dikici <AliRiza.Dikici@kerzner.com>
2019-04-18 17:56:35 +0400

    Regular Commit

--- samples/linear-regression.ts
+++ samples/linear-regression.ts
@@ -5,28 +5,42 @@ export class LinearRegression {
 	public weights: Tensor;
 	private sampleSize = 0;
 	private numberOfFeatures = 0;
-	private mean: number;
-	private stddev: number;
+	private mean: Tensor;
+	private stddev: Tensor;
 
-	constructor( private features: Tensor, private labels: Tensor, private options: LinearRegressionOptions = {} ) {
+	private features: Tensor;
+
+	constructor( features: Tensor, private labels: Tensor, private options: LinearRegressionOptions = {} ) {
+		this.prepareFeatures( features ).then( result => { this.features = result; } );
 		this.options = { learningRate: 0.1, maxIterations: 1000, ...options };
-		this.sampleSize = this.features.shape[0];
-		this.numberOfFeatures = this.features.shape[1];
+		this.sampleSize = features.shape[0];
+		this.numberOfFeatures = features.shape[1];
 		this.weights = zeros( [this.numberOfFeatures, 1] );
-		this.prepareFeatures( features );
 	}
 
 	private prepareFeatures = async ( f: Tensor, shouldSet = false ) => {
-		const { mean, variance } = await moments( f );
-		this.mean = ( await mean.array() as number );
-		this.stddev = Math.pow( ( await variance.array() as number ), .5 );
-		console.log( this.mean, this.stddev, ( await variance.array() ) );
+		if ( !this.mean ) {
+			const { mean, variance } = await moments( f, 0 );
+			this.mean = mean;
+			this.stddev = variance.pow( .5 );
+		}
+		return f.sub( this.mean ).div( this.stddev );
 	}
 
 	public train = async () => {
+		while ( !this.features ) { await this.waiter(); }
+		// this.features.print();
 		for ( let i = 0; i < this.options.maxIterations; i++ ) {
 			await this.gradientDescent();
 		}
+	}
+
+	private waiter = ( timeout = 500 ) => {
+		return new Promise( ( resolve, reject ) => {
+			setTimeout( () => {
+				resolve();
+			}, timeout );
+		} );
 	}
 
 	private gradientDescent = async () => {
@@ -37,11 +51,14 @@ export class LinearRegression {
 	}
 
 	public test = async ( testFeatures: Tensor, testLabels: Tensor ) => {
-		// testFeatures.print();
+		testFeatures = await this.prepareFeatures( testFeatures );
+		// this.weights.print();
 		// testLabels.print();
+		// testFeatures.matMul( this.weights ).print();
+		// testFeatures.print();
 		const sstot = ( await testLabels.sub( testLabels.mean() ).pow( 2 ).sum().array() as number );
 		const ssres = ( await testLabels.sub( testFeatures.matMul( this.weights ) ).pow( 2 ).sum().array() as number );
-		console.log( ssres, 'vs', sstot );
+		// console.log( ssres, 'vs', sstot );
 		const rSquared = 1 - ssres / sstot;
 		return rSquared;
 	}
