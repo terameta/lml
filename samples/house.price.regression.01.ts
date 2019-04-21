256cee48d1ffebe83261283b8a6babb064bfbe2b
AliRiza Dikici <AliRiza.Dikici@kerzner.com>
2019-04-21 17:51:23 +0400

    another day

--- samples/house.price.regression.01.ts
+++ samples/house.price.regression.01.ts
@@ -8,7 +8,13 @@ console.clear();
 
 const run = async () => {
 	const rawDataSet = await readCSV( './datafiles/cars.csv' );
-	const dataSet = rawDataSet.map( d => ( { horsepower: parseFloat( d.horsepower ), mpg: parseFloat( d.mpg ) } ) );
+	const dataSet = rawDataSet.map( d => ( {
+		horsepower: parseFloat( d.horsepower ),
+		weight: parseFloat( d.weight ),
+		displacement: parseFloat( d.displacement ),
+		// acceleration: parseFloat( d.acceleration ),
+		mpg: parseFloat( d.mpg )
+	} ) );
 	// console.log( dataSet );
 
 	const [testSet, trainingSet] = splitDataSet( dataSet, 13 );
@@ -19,15 +25,25 @@ const run = async () => {
 	console.log( '--- We are trying to calculate MPG as a function of Horsepower ---' );
 	console.log( '--- MPG = m * HP + b' );
 	// // console.table( testSet );
-	const features = ones( [trainingSet.length, 1] ).concat( tensor( trainingSet.map( t => [t.horsepower] ) ), 1 );
-	const testFeatures = ones( [testSet.length, 1] ).concat( tensor( testSet.map( t => [t.horsepower] ) ), 1 );
+	const features = ones( [trainingSet.length, 1] ).concat( tensor( trainingSet.map( t => [
+		t.horsepower,
+		t.weight,
+		t.displacement,
+		// t.acceleration
+	] ) ), 1 );
+	const testFeatures = ones( [testSet.length, 1] ).concat( tensor( testSet.map( t => [
+		t.horsepower,
+		t.weight,
+		t.displacement,
+		// t.acceleration
+	] ) ), 1 );
 	const labels = tensor( trainingSet.map( t => [t.mpg] ) );
 	const testLabels = tensor( testSet.map( t => [t.mpg] ) );
 	// // ones( [features.shape[0], 1] ).concat( features, 1 ).print();
 
-	const regression = new LinearRegression( features, labels, { learningRate: .1, maxIterations: 100 } );
+	const regression = new LinearRegression( features, labels, { learningRate: .13, maxIterations: 1000 } );
 	await regression.train();
-	// regression.weights.print();
+	regression.weights.print();
 	// await testFeatures.matMul( regression.weights ).sub( testLabels ).array().then( console.log );
 	console.log( 'R2:', await regression.test( testFeatures, testLabels ) );
 }
